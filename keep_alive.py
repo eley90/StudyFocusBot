cec90da2cf556a480ba3dcdb0e4d2d6523940d59
eley90 <136241425+eley90@users.noreply.github.com>
2023-06-12 13:58:12 +0200

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -7,13 +7,25 @@ app = Flask('')
 
 @app.route('/')
 def home():
+    """
+    Handler for the root URL ('/').
+    Returns a simple message indicating that the server is alive.
+    """
     return "Hello. I am alive!"
 
 def run():
+    """
+    Function to start the Flask server.
+    """
     app.run(host='0.0.0.0', port=8080, debug=False, use_reloader=False)
 
 
 def keep_alive():
+    """
+    Function to keep the Flask server running in the background.
+    Starts a new thread to execute the 'run' function.
+    """
+
     server = Thread(target=run)
     server.start()
 
