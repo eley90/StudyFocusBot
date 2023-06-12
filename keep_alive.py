971fc8cea33891a38dcd809ddd97771007a59a69
eley90 <136241425+eley90@users.noreply.github.com>
2023-06-12 13:54:31 +0200

    Add files via upload

---
+++ keep_alive.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from threading import Thread
+
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return "Hello. I am alive!"
+
+def run():
+    app.run(host='0.0.0.0', port=8080, debug=False, use_reloader=False)
+
+
+def keep_alive():
+    server = Thread(target=run)
+    server.start()
+
+
